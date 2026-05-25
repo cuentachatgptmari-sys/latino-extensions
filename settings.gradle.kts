c1560cbb8204bd5c066baea0237b2ae7248b73f1
Guille <cuentachatgptmari@gmail.com>
2026-05-24 21:06:25 -0400

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "latino-extensions"
+
+include(
+    ":PelisplushдProvider",
+    ":CinecalidadProvider",
+    ":GnulaProvider",
+    ":CliverProvider",
+    ":Cuevana3Provider",
+    ":RepelisplusProvider",
+    ":MegadedeProvider",
+    ":PeliCineHDProvider",
+    ":FilmapikProvider",
+    ":SeriesdankoProvider",
+    ":LaMovieProvider",
+    ":PelisplusProvider",
+    ":Series24Provider",
+    ":TopstreamfilmProvider",
+    ":NetcinezProvider",
+    ":CoflixProvider",
+    ":LatanimeProvider",
+    ":GoojaraProvider",
+    ":IdlixProvider"
+)
