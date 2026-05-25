cf3c5a7a1fe039983f0a5ef8a4499cea5539a6f0
Guille <cuentachatgptmari@gmail.com>
2026-05-24 21:09:32 -0400

    Create build.gradle.kts

---
+++ CinecalidadProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Peliculas y series en HD y 4K latino"
+    authors = listOf("cuentachatgptmari-sys")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://cinecalidad.am/favicon.ico"
+}
