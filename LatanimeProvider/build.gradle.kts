65623340006c1d8cb705864a8389e736bb0df88c
Guille <cuentachatgptmari@gmail.com>
2026-05-24 22:56:53 -0400

    Create build.gradle.kts

---
+++ LatanimeProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Anime en español latino"
+    authors = listOf("cuentachatgptmari-sys")
+    status = 1
+    tvTypes = listOf("Anime", "TvSeries")
+    iconUrl = "https://latanime.org/favicon.ico"
+}
