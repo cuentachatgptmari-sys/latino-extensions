9d70ae8bb5a4ce319213178a0ea6d12c19e9afba
Guille <cuentachatgptmari@gmail.com>
2026-05-24 22:43:21 -0400

    Create build.gradle.kts

---
+++ FilmapikProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Peliculas en HD en español latino"
+    authors = listOf("cuentachatgptmari-sys")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://filmapik.my.id/favicon.ico"
+}
