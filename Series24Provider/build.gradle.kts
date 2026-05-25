4cae2c8934121d120a0e6b2ba24f3626350c22b5
Guille <cuentachatgptmari@gmail.com>
2026-05-24 22:50:08 -0400

    Create build.gradle.kts

---
+++ Series24Provider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Series y peliculas en español latino"
+    authors = listOf("cuentachatgptmari-sys")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://series24.one/favicon.ico"
+}
