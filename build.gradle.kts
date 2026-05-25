5f2ab0838decca492d919fa19587d1a1d135b3ec
Guille <cuentachatgptmari@gmail.com>
2026-05-25 01:54:36 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,9 +47,7 @@ subprojects {
     }
 
     dependencies {
-        val apk by configurations
         val implementation by configurations
-        apk("com.lagradost:cloudstream3:pre-release")
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.1")
         implementation("org.jsoup:jsoup:1.15.3")
