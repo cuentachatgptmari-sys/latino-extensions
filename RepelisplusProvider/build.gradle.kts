0fc106e807c0d1f6df1c860214eb3bd3b648bf72
Guille <cuentachatgptmari@gmail.com>
2026-05-24 22:38:26 -0400

    Create build.gradle.kts

---
+++ RepelisplusProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "es"
+    description = "Peliculas y series en español latino"
+    authors = listOf("cuentachatgptmari-sys")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://repelisplus.com/favicon.ico"
+}
